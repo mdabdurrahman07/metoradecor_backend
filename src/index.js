5f8fed8747146b644487345fa8d6c683e5a76010
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-04-27 23:12:57 +0600

    feat: add cookie-parser and cors dependencies; set up express app

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-import 'dotenv/config';
+import "dotenv/config";
 import connectDB from "./db/connectDB.js";
+import { app } from "./app.js";
 
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed", err);
+  });
