ecf6fd17eccff4ec8847616cf8ae7c381e756531
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-04-28 00:04:47 +0600

    feat: create user model with schema definition for name, email, avatar, password, and refreshToken

---
+++ src/models/user.model.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    avatar: {
+      // use cloudinary
+      type: String,
+      required: false,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", userSchema);
