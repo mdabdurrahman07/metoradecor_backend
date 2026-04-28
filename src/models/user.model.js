e41c74111abecb36030bac94e636cc8b3d3e4707
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-04-28 16:50:05 +0600

    feat: update user model to hash passwords and add password comparison method; update package dependencies

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -25,14 +25,24 @@ const userSchema = new mongoose.Schema(
     refreshToken: {
       type: String,
     },
-    role:{
-      default: "user"
-    }
+    role: {
+      default: "user",
+    },
   },
   {
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 export { User };
