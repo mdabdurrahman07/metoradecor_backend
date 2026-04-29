d44665ad6fea1da1f8fa97dc4daf830d8759453c
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-04-29 23:50:44 +0600

    feat: enhance user registration with error handling and response structure

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,53 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-
+import { apiError } from "../utils/apiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.service.js";
+import { apiResponse } from "../utils/apiResponse.js";
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
+  // get user details from frontend
+  const { email, name, password } = req.body;
+  // validation
+  if ([name, email, password].some((field) => field?.trim() === "")) {
+    throw new apiError(400, "All field fields are required");
+  }
+  // check if user already exists: email
+  const existedUser = User.findOne({ email });
+  if (existedUser) {
+    throw new apiError(409, "User with this email already exists");
+  }
+  // check for images
+  const avatarLocalPath = req.file?.avatar[0]?.path;
+  console.log(req.file?.avatar);
+  if (!avatarLocalPath) {
+    throw new apiError(400, "Avatar file is required");
+  }
+  // upload cloudinary
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  // cloudinary check
+  if (!avatar) {
+    throw new apiError(400, "Avatar uploading error in cloudinary");
+  }
+  // create user object - create entry in db
+  const user = await User.create({
+    name,
+    avatar: avatar?.url,
+    email,
+    password,
   });
+  console.log(user);
+  // remove password and refresh token field from response
+  const createdUser = await User.findOne(user?._id).select(
+    "-password -refreshToken"
+  );
+  // check for user creation
+  if (!createdUser) {
+    throw new apiError(500, "something went wrong while creating a user");
+  }
+
+  // return res
+  return res
+    .status(201)
+    .json(new apiResponse(200, createdUser, "user registered successfully"));
 });
 
 export { registerUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser)
+router.route("/register").post(upload.single({ name: "avatar" }), registerUser);
 // output http://localhost:8000/api/v1/metoradecor/users/register
 export default router;
